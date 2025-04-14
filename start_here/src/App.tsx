cd7850709bcd91f0c14618b3d4b394b0e09e47a0
Andrew Lim <Andrew.Lim@e14.io>
2025-04-14 11:15:56 -0300

    Setup finished

--- start_here/src/App.tsx
+++ start_here/src/App.tsx
@@ -1,27 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
 import TopBar from './components/TopBar';
 
 const App = () => {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/api')
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message));
-  }, []);
-
   return (
-    <div className="min-h-screen bg-[#121212] text-white">
+    <div>
       <TopBar />
-      <div className="flex">
-        {/* Sidebar will go here */}
-        <main className="flex-1 p-4">
-          <h1 className="text-2xl font-semibold">{message}</h1>
-        </main>
-      </div>
-      {/* Footer will go here */}
     </div>
   );
 };
+
+const container = document.getElementById('root');
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
 
 export default App;
